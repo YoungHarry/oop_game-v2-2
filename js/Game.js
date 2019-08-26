5156e95c627cb80427b39af3f4a2996e1c5756c0
Harry Pendleton <youngharry1997@gmail.com>
2019-08-26 13:57:42 -0400

    Initial commit

---
+++ js/Game.js
@@ -0,0 +1,32 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game{
+   constructor(){
+     function createPhrases(){
+       let newPhrases = [
+          {phrase:"Wait till they get a load of me..."},
+          {phrase:"The force will be with you, always..."},
+          {phrase:"How many times do I have to say it?"},
+          {phrase:"Pizza Time ! "},
+          {phrase:"It can't rain all the time..."}
+       ];
+       return newPhrases;
+     };
+        this.missed = 0;
+        this.activePhrase = `null`;
+        this.phrases = createPhrases();
+
+        function getRandomPhrase(){
+          let randomNumber = Math.floor(Math.random()*5)+1;
+                let randomPhrase = newPhrases[randomNumber].phrase;
+                  return randomPhrase;
+
+
+
+            }
+            this.phrases = createPhrases();
+
+        };
+   };
---
+++ js/app.js
@@ -0,0 +1,17 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * app.js */
+//const phrase = new Phrase('Life is like a box of chocolates');
+//console.log(`${phrase.phrase}`);
+//const game = new Game();
+
+//game.phrases.forEach((phrase, index)=>{
+  //console.log(`${index}- phrase: ${phrase.phrase}`);
+
+//})
+const logPhrase = (phrase) => {
+  console.log(`phrase.phrase`);
+
+};
+const game = new Game();
+logPhrase(game.getRandomPhrase());
