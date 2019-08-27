f9a22903ffff12cc739a3342020a045350434913
Harry Pendleton <youngharry1997@gmail.com>
2019-08-26 21:19:06 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,15 +3,9 @@
  * app.js */
 //const phrase = new Phrase('Life is like a box of chocolates');
 //console.log(`${phrase.phrase}`);
-//const game = new Game();
+const game = new Game();
 
 //game.phrases.forEach((phrase, index)=>{
   //console.log(`${index}- phrase: ${phrase.phrase}`);
 
 //})
-const logPhrase = (phrase) => {
-  console.log(`phrase.phrase`);
-
-};
-const game = new Game();
-logPhrase(game.getRandomPhrase());
