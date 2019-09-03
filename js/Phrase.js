514c548c14ecf2f188de0fe9360eaf53743a2a2d
Harry Pendleton <youngharry1997@gmail.com>
2019-09-03 12:42:04 -0400

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,7 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
-
+"use strict"
  class Phrase{
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
@@ -9,6 +9,30 @@
 
 
     }
+
+
+
+    /* Display the phrase on screen/the html
+    1.Grabbing the div element
+    Insert the test into inner html?
+    Inserting the random phrase into that div element
+    Hide the phrase from the player
+    Add on click
+    So grab the Start game button and also have an event listener
+    Make the phrase equal the elements on the html
+    Then using a function showing the right letter if
+    the player guesses it right.
+
+    */
+  /*  addPhrasetoDisplay(){
+      //let getDiv = $('#phrase')
+      //let getDivKeys = $("")
+      console.log(getRandomPhrase().split());
+
+
+
+
+    } */
 
 
 
